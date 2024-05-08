6f49f4871ed4399df74f3efaa5a862f2d66395a9
Chriscloudaz <chriscloudaz@gmail.com>
2024-05-08 21:44:53 +0100

    updated db url

--- EcommerceApp/src/main/java/com/conn/DBConnect.java
+++ EcommerceApp/src/main/java/com/conn/DBConnect.java
@@ -16,7 +16,8 @@ public class DBConnect
 		//	conn = DriverManager.getConnection("jdbc:sqlite:mydatabase.db");
 		//	conn = DriverManager.getConnection("jdbc:sqlite:C:/Users/Swapnil/eclipse-workspace/Online Electronic Shopping/mydatabase.db");
 		//	conn = DriverManager.getConnection("jdbc:mysql://db:3306/ecommerce_app","root","root");
-			conn = DriverManager.getConnection("jdbc:mysql://mysql-service.ecommerce-app-db.svc.cluster.local:3306/ecommerce_app","root","root");
+		//	conn = DriverManager.getConnection("jdbc:mysql://mysql-service.ecommerce-app-db.svc.cluster.local:3306/ecommerce_app","root","root");
+			conn = DriverManager.getConnection("jdbc:mysql://database-1.csa0snvktt2c.eu-north-1.rds.amazonaws.com:3306/ecommerce_app","admin","3NbAzXbvJFcGVYwZH04h");
 
 			
 		}catch (Exception e) {
