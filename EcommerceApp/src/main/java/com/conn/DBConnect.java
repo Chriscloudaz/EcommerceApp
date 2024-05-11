080c51615539bd57ac2165be5a4fd1c075eaeebf
Chriscloudaz <chriscloudaz@gmail.com>
2024-05-11 18:30:56 +0100

    updated db url

--- EcommerceApp/src/main/java/com/conn/DBConnect.java
+++ EcommerceApp/src/main/java/com/conn/DBConnect.java
@@ -18,7 +18,6 @@ public class DBConnect
 		//	conn = DriverManager.getConnection("jdbc:mysql://db:3306/ecommerce_app","root","root");
 		//	conn = DriverManager.getConnection("jdbc:mysql://mysql-service.ecommerce-app-db.svc.cluster.local:3306/ecommerce_app","root","root");
 		//	conn = DriverManager.getConnection("jdbc:mysql://database-1.csa0snvktt2c.eu-north-1.rds.amazonaws.com:3306/ecommerce_app","admin","3NbAzXbvJFcGVYwZH04h");
-			conn = DriverManager.getConnection("jbdc:mysql://db-mysql-lon1-56322-do-user-15714856-0.c.db.ondigitalocean.com:25060/ecommerce_app","ecommerce_user","AVNS_Be4krs4TIVTTf2-ssJQ");
 			
 		}catch (Exception e) {
 			e.printStackTrace();
